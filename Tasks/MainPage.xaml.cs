6e3decb211a6bb1533ea390a0b9124da01a0cbf1
uzytkownik <matlagkacper@gmail.com>
2023-12-12 20:12:23 +0100

    Add project files.

---
+++ Tasks/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tasks.Model;
+using Xamarin.Forms;
+
+namespace Tasks {
+    public partial class MainPage : ContentPage {
+        List<TaskModel> tasks = new List<TaskModel>();
+        public MainPage() {
+            InitializeComponent();
+            tasks = new List<TaskModel>() {
+                new TaskModel() {
+                    ID=new Guid(),
+                    Title="title",
+                    Description="description",
+                    Importance="Ważne",
+                }
+            };
+            TasksList.ItemsSource = tasks;
+        }
+
+        private void Add_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void Edit_Clicked(object sender, EventArgs e) {
+
+        }
+
+        private void Delete_Clicked(object sender, EventArgs e) {
+
+        }
+    }
+}
---
+++ Tasks/Model/TaskModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Model {
+    public class TaskModel {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Importance { get; set; }
+        public string Image {
+            get {
+                if (Importance == "Ważne")
+                    return "https://upload.wikimedia.org/wikipedia/commons/6/62/Nuvola_apps_important.png";
+                if (Importance == "Bardzo Ważne")
+                    return "https://static.vecteezy.com/system/resources/previews/021/433/032/non_2x/important-rubber-stamp-free-png.png";
+                return "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Info_icon_001.svg/2048px-Info_icon_001.svg.png";
+            }
+        }
+    }
+}
